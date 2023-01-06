0990d00b28a20f1fbef1888cfb3bc42d3c601103
Ifeoluwa <onabajoif@yahoo.com>
2023-01-05 17:58:58 -0800

    volla

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-from sys import *
-if __name__ = "__main__":
-    count = len(argv) - 1
-    if count < 1:
-        print("{} arguments.".format(count))
-    elif count == 1:
-        print("{} argument:".format(count))
-        print("{}: {}".format(count, argv[count]))
+import sys
+
+
+def main():
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(count))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        print("{:d} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+
+
+if __name__ == "__main__":
+    main()
