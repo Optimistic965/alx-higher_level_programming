8b5b4e1be3166872312bae5c2a4d65baa11f881f
Ifeoluwa <onabajoif@yahoo.com>
2023-01-05 17:47:49 -0800

    volla

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from sys import *
-if len(argv) - 1 < 1:
-    print("{} arguments.".format(len(argv) - 1))
-else:
-    print("{} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+if __name__ = "__main__":
+    if len(argv) - 1 < 1:
+        print("{} arguments.".format(len(argv) - 1))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
